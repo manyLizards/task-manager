6dd3c4cbbb0dd551e65db9977ff264beaa553e31
Jordan <jnbarringer96@gmail.com>
2023-02-12 03:34:20 -0600

    created task model, created empty arrays for each day, fleshed out index.html

---
+++ models/Tasks.js
@@ -0,0 +1,13 @@
+let taskList = [];
+
+class Task {
+    constructor(taskName, important, dueDate) {
+        this.taskName = taskName;
+        this.important = important;
+        this.dueDate = ;
+        this.importanceLevel = 0
+        if (important = true) {
+            this.importanceLevel + 1
+        }
+    }
+}
